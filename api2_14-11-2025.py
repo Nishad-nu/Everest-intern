c42208d70af8630521a9094587716c594872c4b3
Nishad-nu <nishatnikhatnayeem@gmail.com>
2025-11-14 18:37:00 +0530

    7 file

--- api2_14-11-2025.py
+++ api2_14-11-2025.py
@@ -1,4 +1,25 @@
 import requests
+import json
+from collections import Counter
 url = "https://dummy-json.mock.beeceptor.com/users"
 responce = requests.get (url)
-print(responce)
+data = responce.json()
+with open("data.json","w") as f:
+    json.dump(data,f)
+print("json.data saved to data.json")
+
+print(type(data))
+#method - 1
+country_count = {}
+for i in data:
+    country = i.get("country")
+    if country in country_count:
+        country_count[country] += 1
+    else:
+        country_count[country] = 1
+print(country_count)
+
+#method - 2
+country_list = [ i.get("country") for i in data if i.get("country")]
+country_count = Counter(country_list)
+print(dict(country_count))
